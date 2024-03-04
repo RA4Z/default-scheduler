ec19c60f359c66c5a06a224e2fe1d6e1d424649e
RA4Z <rraz639@gmail.com>
2024-03-04 09:35:43 -0300

    Alertando erro ao não conseguir acessar transação

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from sap_functions import SAP
 
 sap = SAP(0)
-sap.select_transaction('CN47N')
+sap.select_transaction('CN47Ngsgseg')
--- sap_functions.py
+++ sap_functions.py
@@ -1,4 +1,5 @@
 import win32com.client
+from tkinter import messagebox
 #module SAP Functions, development started in 2024/03/01
 class SAP():
     def __init__(self, window: int):
@@ -12,4 +13,9 @@ class SAP():
         if self.session.activeWindow.name == 'wnd[1]':
             self.session.findById("wnd[1]/usr/ctxtTCNT-PROF_DB").Text = "000000000001"
             self.session.findById("wnd[1]/tbar[0]/btn[0]").press()
-
+        if not self.session.info.transaction == transaction:
+            messagebox.showerror(title='Erro ao Selecionar Transação', message=self.get_footer_message())
+            exit()
+    
+    def get_footer_message(self):
+        return(self.session.findById("wnd[0]/sbar").Text)
