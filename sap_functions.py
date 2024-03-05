f843da59323cace6d4fcaec65172f685a7c39a88
RA4Z <rraz639@gmail.com>
2024-03-05 18:05:45 -0300

    Criando comentários básicos em cada função

--- sap_functions.py
+++ sap_functions.py
@@ -5,6 +5,8 @@ import os
 
 #module SAP Functions, development started in 2024/03/01
 class SAP():
+    
+    # Initializes the SAP object with a specified window index.
     def __init__(self, window: int):
         sapguiauto = win32com.client.GetObject('SAPGUI')
         application = sapguiauto.GetScriptingEngine
@@ -12,12 +14,14 @@ class SAP():
         self.session = connection.Children(window)
         self.window = self.__active_window()
 
+    # Finds the active window index.
     def __active_window(self):
         regex = re.compile('[0-9]')
         matches = regex.findall(self.session.ActiveWindow.name)
         for match in matches:
             return match
 
+    # Scrolls through tabs within a specified area.
     def __scroll_through_tabs(self, area, extension, selected_tab):
         children = area.Children
         for child in children:
@@ -36,6 +40,7 @@ class SAP():
                 return area
         return area
     
+    # Scrolls through a grid based on its extension.
     def __scroll_through_grid(self, extension):
         if self.session.findById(extension).Type == 'GuiShell': 
             try:
@@ -63,6 +68,7 @@ class SAP():
                 result = self.__scroll_through_grid(extension + '/' + children[i].name)
         return result
 
+    # Scrolls through fields within a specified extension and objective.
     def __scroll_through_fields(self, extension, objective, selected_tab):
         children = self.session.findById(extension).Children
         result = False
@@ -84,6 +90,7 @@ class SAP():
                 result = self.__scroll_through_fields(extension + "/" + children[i].name, objective, selected_tab)
         return result
 
+    # Contains generic conditional statements for different objectives.
     def __generic_conditionals(self, index, children, objective):
         if objective == 'write_text_field':
             if children(index).Text == self.field_name:
@@ -171,6 +178,7 @@ class SAP():
             return
         return False
 
+    # Selects a transaction within the SAP session.
     def select_transaction(self, transaction):
         self.session.startTransaction(transaction)
         if self.session.activeWindow.name == 'wnd[1]':
@@ -180,12 +188,14 @@ class SAP():
             messagebox.showerror(title='Error selecting transaction', message=self.get_footer_message())
             exit()
     
+    # Selects the main screen of the SAP session.
     def select_main_screen(self):
         if not self.session.info.transaction == "SESSION_MANAGER":
             self.session.startTransaction('SESSION_MANAGER')
             if self.session.ActiveWindow.name == "wnd[1]":
                 self.session.findById("wnd[1]/tbar[0]/btn[0]").press()
 
+    # Cleans all fields within the SAP session.
     def clean_all_fields(self, selected_tab = 0):
         self.window = self.__active_window()
         area = self.__scroll_through_tabs(self.session.findById(f"wnd[{self.window}]/usr"), f"wnd[{self.window}]/usr", selected_tab)
@@ -197,6 +207,7 @@ class SAP():
                 except Exception as e:
                     print(f'The error {e} has happenned!')
 
+    # Inserts a variant into the SAP session.
     def insert_variant(self, variant_name):
         try:
             self.session.findById("wnd[0]/tbar[1]/btn[17]").press()
@@ -209,6 +220,7 @@ class SAP():
         except Exception as e:
             print(f'The error {e} has happenned!')
 
+    # Changes the active tab within the SAP session.
     def change_active_tab(self, selected_tab):
         self.window = self.__active_window()
         area = self.__scroll_through_tabs(self.session.findById(f"wnd[{self.window}]/usr"), f"wnd[{self.window}]/usr", selected_tab)
@@ -217,7 +229,8 @@ class SAP():
         except Exception as e:
             print(f'The error {e} has happenned!')
         return
-
+    
+    # Writes text into a text field within the SAP session.
     def write_text_field(self, field_name, desired_text, target_index=0, selected_tab=0):
         self.window = self.__active_window()
         self.field_name = field_name
@@ -226,6 +239,7 @@ class SAP():
         if selected_tab > 0: self.change_active_tab(selected_tab)
         return self.__scroll_through_fields(f"wnd[{self.window}]/usr", 'write_text_field', selected_tab)
 
+    # Writes text into a text field until a certain index within the SAP session.
     def write_text_field_until(self, field_name, desired_text, target_index=0, selected_tab=0):
         self.window = self.__active_window()
         self.field_name = field_name
@@ -234,6 +248,7 @@ class SAP():
         if selected_tab > 0: self.change_active_tab(selected_tab)
         return self.__scroll_through_fields(f"wnd[{self.window}]/usr", 'write_text_field_until', selected_tab)
 
+    # Flags a field within the SAP session.
     def flag_field(self, field_name, desired_operator: bool, target_index=0, selected_tab=0):
         self.window = self.__active_window()
         self.field_name = field_name
@@ -242,6 +257,7 @@ class SAP():
         if selected_tab > 0: self.change_active_tab(selected_tab)
         return self.__scroll_through_fields(f"wnd[{self.window}]/usr", 'flag_field', selected_tab)
 
+    # Selects an option within a field in the SAP session.
     def option_field(self, field_name, target_index=0, selected_tab=0):
         self.window = self.__active_window()
         self.field_name = field_name
@@ -249,6 +265,7 @@ class SAP():
         if selected_tab > 0: self.change_active_tab(selected_tab)
         return self.__scroll_through_fields(f"wnd[{self.window}]/usr", 'option_field', selected_tab)
     
+    # Presses a button within the SAP session.
     def press_button(self, field_name, target_index=0, selected_tab=0):
         self.window = self.__active_window()
         self.field_name = field_name
@@ -256,12 +273,14 @@ class SAP():
         if selected_tab > 0: self.change_active_tab(selected_tab)
         return self.__scroll_through_fields(f"wnd[{self.window}]", 'press_button', selected_tab)
     
+    # Finds a text field within the SAP session.
     def find_text_field(self, field_name, selected_tab = 0):
         self.window = self.__active_window()
         self.field_name = field_name
         if selected_tab > 0: self.change_active_tab(selected_tab)
         return self.__scroll_through_fields(f"wnd[{self.window}]/usr", 'find_text_field', selected_tab)
-
+    
+    # Selects multiple entries within a field in the SAP session.
     def multiple_selection_field(self, field_name, target_index=0, selected_tab=0):
         self.window = self.__active_window()
         self.field_name = field_name
@@ -269,6 +288,7 @@ class SAP():
         if selected_tab > 0: self.change_active_tab(selected_tab)
         return self.__scroll_through_fields(f"wnd[{self.window}]/usr", 'multiple_selection_field', selected_tab)
 
+    # Pastes data into multiple selection fields in the SAP session.
     def multiple_selection_paste_data(self, data):
         try:
             with open('temp_paste.txt', 'w') as arquivo:
@@ -283,6 +303,7 @@ class SAP():
         except Exception as e:
             print(f'The error {e} has happenned!')
 
+    # Saves a file in the SAP session.
     def save_file(self, file_name, path, option=0, type_of_file='txt'):
         if 'xls' in type_of_file:
             self.session.findById("wnd[0]/mbar/menu[0]/menu[1]/menu[1]").Select()
@@ -296,14 +317,17 @@ class SAP():
         self.session.findById("wnd[1]/usr/ctxtDY_FILENAME").Text = f'{file_name}.{type_of_file}'
         self.session.findById("wnd[1]/tbar[0]/btn[11]").press()
 
+    # Views data in list form within the SAP session.
     def view_in_list_form(self):
         myGrid = self.get_my_grid()
         myGrid.pressToolbarContextButton("&MB_VIEW")
         myGrid.SelectContextMenuItem("&PRINT_BACK_PREVIEW")
 
+    # Retrieves the grid object within the SAP session.
     def get_my_grid(self):
         self.window = self.__active_window()
         return self.__scroll_through_grid(f'wnd[{self.window}]/usr')
 
+    # Retrieves the footer message within the SAP session.
     def get_footer_message(self):
         return(self.session.findById("wnd[0]/sbar").Text)
