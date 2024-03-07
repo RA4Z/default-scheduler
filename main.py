77d3b61d963bb16d348422a657089bccb11fd196
RA4Z <rraz639@gmail.com>
2024-03-07 15:24:08 -0300

    Criando loop básico para funcionamento da macro

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from statements import State
 script = State()
 script.app.mainloop()
 
-if script.app.result: 
-    print(script.app.data)
-    script.sap.select_transaction('MD4C')
+if script.app.result:
+    all_data = str(script.app.data).split('\n')
+
+    for data in all_data:
+        if data.strip() != '':
+            pass    #PUT YOUR CODE THERE
